58b4ca53478b70c12334dd319383094d9364f45a
Bruno <bruno.dev8801@gmail.com>
2022-01-30 14:44:50 -0300

    created stopwatch

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,25 +1,114 @@
-let digitalElement = $("main #content .relogio .timeNumber");
-let secondElement = $("main #content .relogio .circle .p_s");
-let minuteElement = $("main #content .relogio .circle .p_m");
-let hourElement = $("main #content .relogio .circle .p_h");
+function updatePage() {
+  // Relógio
+  let digitalElement = $("main #content .relogio .timeNumber");
+  let secondElement = $("main #content .relogio .circle .p_s");
+  let minuteElement = $("main #content .relogio .circle .p_m");
+  let hourElement = $("main #content .relogio .circle .p_h");
 
-function viewTimeScreen() {
-  let date = new Date();
-  let hour = date.getHours();
-  let minute = date.getMinutes();
-  let second = date.getSeconds();
+  function viewTimeScreen() {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
 
-  digitalElement.html(`${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`);
+    digitalElement.html(
+      `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`
+    );
 
-  hDeg = (360 / 12) * hour - 90;
-  hourElement.css("transform", `rotate(${hDeg}deg)`);
-  mDeg = (360 / 60) * minute - 90;
-  minuteElement.css("transform", `rotate(${mDeg}deg)`);
-  sDeg = (360 / 60) * second - 90;
-  secondElement.css("transform", `rotate(${sDeg}deg)`);
+    hDeg = (360 / 12) * hour - 90;
+    hourElement.css("transform", `rotate(${hDeg}deg)`);
+    mDeg = (360 / 60) * minute - 90;
+    minuteElement.css("transform", `rotate(${mDeg}deg)`);
+    sDeg = (360 / 60) * second - 90;
+    secondElement.css("transform", `rotate(${sDeg}deg)`);
+  }
+  function fixZero(time) {
+    return time < 10 ? `0${time}` : time;
+  }
+
+  setInterval(viewTimeScreen, 1000);
+
+  // Cronômetro
+  let timerStopwatch = $("main #content .cronometro .timer span");
+  let hourStopwatch = 0;
+  let minuteStopwatch = 0;
+  let secondStopwatch = 0;
+
+  function updateTimerStopwatch() {
+    if (secondStopwatch >= 60) {
+      secondStopwatch = 0;
+      minuteStopwatch++;
+    }
+    if (minuteStopwatch >= 60) {
+      minuteStopwatch = 0;
+      hourStopwatch++;
+    }
+    if (hourStopwatch > 24) {
+      hourStopwatch = 0;
+      minuteStopwatch = 0;
+      secondStopwatch = 0;
+      stopTimerStopwatch();
+    }
+    secondStopwatch++;
+
+    timerStopwatch.html(
+      `${fixZero(hourStopwatch)}:${fixZero(minuteStopwatch)}:${fixZero(
+        secondStopwatch
+      )}`
+    );
+  }
+  let interval;
+
+  $("main #content .cronometro .btn-timer button.start").click(() => {
+    interval = setInterval(updateTimerStopwatch, 1000);
+    $("main #content .cronometro .btn-timer button.start").css(
+      "display",
+      "none"
+    );
+    $("main #content .cronometro .btn-timer button.stop").css(
+      "display",
+      "inline-block"
+    );
+    $("main #content .cronometro .btn-timer button.restart").css(
+      "display",
+      "inline-block"
+    );
+  });
+  $("main #content .cronometro .btn-timer button.stop").click(() => {
+    clearInterval(interval);
+
+    $("main #content .cronometro .btn-timer button.start")
+      .css("display", "inline-block")
+      .html("Continuar");
+    $("main #content .cronometro .btn-timer button.restart").css(
+      "display",
+      "inline-block"
+    );
+    $("main #content .cronometro .btn-timer button.stop").css(
+      "display",
+      "inline-block"
+    );
+  });
+  $("main #content .cronometro .btn-timer button.restart").click(() => {
+    clearInterval(interval);
+    timerStopwatch.html("00:00:00");
+    hourStopwatch = 0;
+    minuteStopwatch = 0;
+    secondStopwatch = 0;
+
+    $("main #content .cronometro .btn-timer button.start").css(
+      "display",
+      "inline-block"
+    );
+    $("main #content .cronometro .btn-timer button.stop").css(
+      "display",
+      "none"
+    );
+    $("main #content .cronometro .btn-timer button.restart").css(
+      "display",
+      "none"
+    );
+  });
 }
-function fixZero(time) {
-  return time < 10 ? `0${time}` : time;
-}
 
-setInterval(viewTimeScreen, 1000);
+updatePage();
---
+++ scripts/navigation.js
@@ -0,0 +1,51 @@
+let content = $("main #content");
+function appClock() {
+  content.html(`
+    <div class="relogio">
+    <div class="circle">
+      <div class="center"></div>
+      <div class="p_h"></div>
+      <div class="p_m"></div>
+      <div class="p_s"></div>
+
+      <div class="risco top">12</div>
+      <div class="risco left">9</div>
+      <div class="risco bottom">6</div>
+      <div class="risco right">3</div>
+    </div>
+
+    <div class="timeNumber">--:--:--</div>
+  </div>`);
+}
+appClock();
+$("header nav ul li:nth-child(1)").click(() => {
+  $("header nav ul li:nth-child(2)").removeClass("activePage");
+  $("header nav ul li:nth-child(3)").removeClass("activePage");
+  $("header nav ul li:nth-child(1)").addClass("activePage");
+  appClock();
+  updatePage();
+});
+
+function appStopwatch() {
+  content.html(`
+        <div class="cronometro">
+          <div class="timer">
+            <span>00:00:00</span>
+          </div>
+          <div class="btn-timer">
+            <button class="stop">Parar</button>
+            <button class="start">Iniciar</button>
+            <button class="restart">Reiniciar</button>
+          </div>
+        </div>
+    `);
+}
+$("header nav ul li:nth-child(2)").click(() => {
+  $("header nav ul li:nth-child(1)").removeClass("activePage");
+  $("header nav ul li:nth-child(3)").removeClass("activePage");
+  $("header nav ul li:nth-child(2)").addClass("activePage");
+  appStopwatch();
+  updatePage();
+});
+
+function appTimer() {}
